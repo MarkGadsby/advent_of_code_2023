cff49cbf9959dbb0be4dbdadc5ad910d3157cb4f
mark.r.gadsby <mark.r.gadsby@gmail.com>
2023-12-02 08:04:32 +0000

    Day 2

---
+++ Day_03_Solve.cs
@@ -0,0 +1,17 @@
+class SolveDayThree : SolutionsBase
+{   
+    public SolveDayThree(StreamReader given_sr) : base(given_sr)
+    { 
+    }
+
+    public override void part_one()
+    {
+        Console.WriteLine($"\t\tSolveDayThree: part_one.\n");
+    }
+    public override void part_two()
+    {
+        resetStream();
+
+        Console.WriteLine($"\t\tSolveDayThree: part_two.\n");
+    }    
+}
